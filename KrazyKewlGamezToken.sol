015c69371be8218981c7b5d5232a6d50949134cd
KrazyKewl <krazykewlgameztoken@gmail.com>
2020-01-01 20:32:40 -0500

    Update KrazyKewlGamezToken.sol

--- KrazyKewlGamezToken.sol
+++ KrazyKewlGamezToken.sol
@@ -2,137 +2,85 @@ pragma solidity ^0.4.20;
 
 
 contract KrazyKewlGamezToken {
-    // Public variables of the token
-    string public name;
-    string public symbol;
-    uint8 public decimals = 8;
-    // 18 decimals is the strongly suggested default, avoid changing it
-    uint256 public totalSupply;
+    uint totalItems;
 
-    // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    // This generates a public event on the blockchain that will notify clients
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    // This generates a public event on the blockchain that will notify clients
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    // This notifies clients about the amount burnt
-    event Burn(address indexed from, uint256 value);
-
-    /**
-     * Constructor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
-    uint256 initialSupply = 100000000;
-    string tokenName = 'KrazyKewlGamez';
-    string tokenSymbol = 'KKGT';
-    constructor() public {
-
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
+    struct Item {
+        uint id;
+        string name;
+        uint price;
+        bool available;
+        address seller;
+        address buyer;
+        bool exists;
     }
 
-    /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != 0x0);
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
-        // Check for overflows
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
-        balanceOf[_from] -= _value;
-        // Add the same to the recipient
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    mapping (uint => Item) items;
+
+    event Purchased(uint id, string name, address indexed seller, address indexed buyer, uint price);
+    event Added(uint id, string name, uint price, address indexed seller, bool available, bool exists);
+    event Total(uint totalItems);
+    event Availability(bool available);
+
+    constructor () public {
+        totalItems = 0;
     }
 
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
-        return true;
+    function checkItemsTotal() public returns (uint total) {
+        emit Total(totalItems);
+        return totalItems;
     }
 
-    /**
-     * Transfer tokens from other address
-     *
-     * Send `_value` tokens to `_to` on behalf of `_from`
-     *
-     * @param _from The address of the sender
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
+    function addItem (string _name, uint _price) public returns (bool success, uint id, string name, uint price, address seller, bool available) {
+        uint itemId = totalItems;
+
+        require(!items[itemId].exists, "An item already exists at this ID.");
+        require(bytes(_name).length > 0, "Item name cannot be empty.");
+        require(_price > 0, "Price must be greater than zero (0).");
+
+        address sellerAddress = msg.sender;
+
+
+        items[itemId] = Item({
+            id: itemId,
+            name: _name,
+            available: true,
+            price: (_price * 1000000), // The conversion for TRX to sun is 1 : 1000000
+            seller: sellerAddress,
+            buyer: 0,
+            exists: true
+        });
+
+        totalItems += 1;
+
+        emit Added(items[itemId].id, items[itemId].name, items[itemId].price, items[itemId].seller, items[itemId].available, items[itemId].exists);
+        return (true, items[itemId].id, items[itemId].name, items[itemId].price, items[itemId].seller, items[itemId].available);
     }
 
-    /**
-     * Set allowance for other address
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     */
-    function approve(address _spender, uint256 _value) public
-    returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+    function checkItem(uint _id) public returns (uint itemId, string name, uint price, bool available,  address seller, address buyer, bool exists) {
+      emit Availability(items[_id].available);
+      return (items[_id].id, items[_id].name, items[_id].price, items[_id].available, items[_id].seller, items[_id].buyer, items[_id].exists);
     }
 
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
-        emit Burn(msg.sender, _value);
-        return true;
+    function buyItem(uint _id) public payable returns (bool success, uint id, string name, address  seller, address  buyer, uint price) {
+        require(items[_id].exists == true, "This item does not exist. Please check the id and try again.");
+        require(items[_id].available == true, "This item is no longer available.");
+        require(items[_id].seller != 0, "This item has no seller");
+        require(items[_id].buyer == 0, "This item is no longer available");
+        require(items[_id].price == msg.value, "Not enough TRX to buy this item.");
+
+        address _buyerAddress = msg.sender;
+
+        _handlePurchase(_id, _buyerAddress, msg.value);
+
+        emit Purchased(_id, items[_id].name, items[_id].seller, items[_id].buyer, items[_id].price);
+
+        return (true, _id, items[_id].name, items[_id].seller, items[_id].buyer, items[_id].price);
     }
 
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        emit Burn(_from, _value);
-        return true;
+    function _handlePurchase(uint _id, address _buyerAddress, uint _value) internal {
+        items[_id].available = false;
+        items[_id].buyer = _buyerAddress;
+        items[_id].seller.transfer(_value);
     }
+
 }
